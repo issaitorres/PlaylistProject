39def96b1be05282a104d2888e36f76bc4fa9a75
Issai Torres <issaitorres@gmail.com>
2023-10-03 22:06:22 -0700

    update filename extension

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/home.js';
+import Home from './pages/home/home';
 import Register from './pages/auth/register';
 import Login from './pages/auth/login';
 import Logout from './pages/auth/logout';
