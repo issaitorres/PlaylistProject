b73ca1e1dc71670189c23e4e5943aa59f36129eb
Issai Torres <issaitorres@gmail.com>
2023-10-28 19:18:04 -0700

    add IconButton from portfolio website to footer for icon links, add new links to footer, update css styles, update footer styles, new account link in footer for account page

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,17 +1,18 @@
 import { useCookies } from 'react-cookie'
 import { Link } from "react-router-dom"
+import IconButton from '../IconButton/IconButton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpotify, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faSpotify, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import glassesIcon from "../../Assets/glasses.png"
 import "./Footer.css"
 
 const Footer = () => {
   const [cookies] = useCookies(["access_token"])
 
-
   return (
     <div className="footer">
       <div className="footer-flex">
-        <div className='footer-link-section'>
+        <div className='footer__link-section'>
           <div>
             <Link to="/" >Home </Link>
           </div>
@@ -27,22 +28,37 @@ const Footer = () => {
                   </div>
                 </>
               :
+              <>
+                <div>
+                  <Link to="/user">Account </Link>
+                </div>
                 <div>
                   <Link to="/logout">Logout </Link>
                 </div>
+              </>
           }
         </div>
-        <div className='footer-link-section'>
-          <div>
-            <a href="https://developer.spotify.com/documentation/web-api" rel="noreferrer" target="_blank"> Powered by the <br></br>Spotify Web API</a>
+        <div className='footer__link-section'>
+          <div className="footer__right">
+            <a href="https://developer.spotify.com/documentation/web-api" rel="noreferrer" target="_blank"> Powered by the <br/>Spotify Web API</a>
           </div>
-          <div>
-            <a href="https://open.spotify.com/" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faSpotify} className="footer-icon" />
-            </a>
-            <a href="https://github.com/issaitorres/PlaylistProject" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} className="footer-icon" />
-            </a>
+          <div className="footer__icon-container">
+            <IconButton
+              href="https://issaitorresportfolio.onrender.com"
+              icon={<img src={glassesIcon} width="40px" height="40px" className="footer__portofolio-icon"/>}
+            />
+            <IconButton
+              href="https://github.com/issaitorres/PlaylistProject"
+              icon={<FontAwesomeIcon icon={faGithub} />}
+            />
+            <IconButton
+              href="https://www.linkedin.com/in/issaitorres"
+              icon={<FontAwesomeIcon icon={faLinkedin} />}
+            />
+            <IconButton
+              href="https://open.spotify.com"
+              icon={<FontAwesomeIcon icon={faSpotify} />}
+            />
           </div>
         </div>
       </div>
